37b28cbd8a619d634c1c20b707c42a23d10af397
Houzl <zhilonghou@gmail.com>
2016-12-08 15:28:20 -0500

    Trying RDD

--- GraphBuilder/src/main/scala-2.11/csye7200/DataFramesBuilder.scala
+++ GraphBuilder/src/main/scala-2.11/csye7200/DataFramesBuilder.scala
@@ -1,11 +1,10 @@
 package csye7200
 
-import org.apache.spark.sql.execution.columnar.MAP
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.storage.StorageLevel
 
 import scala.annotation.tailrec
-import scala.util.{Failure, Success, Try}
+import scala.util.Try
 
 /**
   * Created by houzl on 11/18/2016.
@@ -70,4 +69,81 @@ object DataFramesBuilder{
     }
     spark.createDataFrame(bfsTraversal(List(1L),1,maxLevel, Map(1L -> "/")).toSeq).toDF("id","path")
   }
+
+  def buildPathToRootDF2(edParentDF: DataFrame, spark : SparkSession, maxLevel: Int): DataFrame = {
+    /**
+      * tailrec to build pathToRoot Map
+      * @param dst List of ids
+      * @param level current level
+      * @param maxLevel Max level to travel. -1 means travel all nodes.
+      * @return List of ids for dst's Children.
+      */
+    @tailrec def bfsTraversal(dst: RDD[Long], level: Int, maxLevel: Int, p : Map[Long, String]): Map[Long, String] = {
+      //nextLevelTuple List of (id, List(ids for children))
+      val nextLevelTuple = dst.map(id => (id, edParentDF.filter(s"dst = $id").select("src").rdd.map(_.getLong(0))))
+      val newRDD = for (t <- nextLevelTuple) yield{
+        val parentId = t._1
+        val parentPath = p.getOrElse(parentId,"/")
+        for (id <- t._2) yield (id -> s"$parentPath$parentId/")
+      }
+      val newMap = newRDD.flatMap(_.collect()).collectAsMap().toMap ++ p
+      val nextLevelIds = newRDD.flatMap(_.collect().map(_._1))
+      if (nextLevelIds.isEmpty || (maxLevel != -1 && level >= maxLevel)) newMap
+      else bfsTraversal(nextLevelIds,level + 1,maxLevel, newMap)
+    }
+    val map = bfsTraversal(spark.sparkContext.parallelize(List(1L)),1,maxLevel, Map(1L -> "/"))
+    spark.createDataFrame(map.toSeq).toDF("id","path")
+  }
+}
+object test extends App{
+    val spark = SparkSession
+      .builder()
+      .appName("CSYE 7200 Final Project")
+      .master("local[2]")
+      .config("spark.some.config.option", "some-value")
+      .getOrCreate()
+    val path = "C:\\Users\\houzl\\Downloads\\taxdmp\\"
+    val edgesPath = path + "nodes.dmp"
+    val edParentDF = DataFramesBuilder.getEdgesParentDF(edgesPath, spark)
+    val edDF = edParentDF.getOrElse(spark.createDataFrame(Nil))
+    val dst = spark.sparkContext.parallelize(Seq((10239L,"/1/"),(131567L,"/1/")))
+    val dst2 = List((10239L,"/1/"),(131567L,"/1/"))
+    for (i <- dst) println(i)
+    val id = 1L
+
+    val nextLevelDF = dst2.map(i => {
+      import org.apache.spark.sql.functions.lit
+      val id = i._1
+      val parentPath = i._2
+      edDF.filter(s"dst = $id").select("src").withColumn("parent", lit(s"$parentPath$id/"))
+    })
+  edDF.
+    //for (i <- nextLevelDF) i.show()
+
+
+
+//  val nextLevelTuple = dst.map(i => {
+//    import org.apache.spark.sql.functions.lit
+//    val id = i._1
+//    val parentPath = i._2
+//    val rankUDF = udf((p:String) => p)
+//    (s"$parentPath$id/" , edDF.filter(s"dst = $id").select("src").withColumn("parent", lit(s"$parentPath$id/")).rdd.map(_.getLong(0)))
+//  })
+//  for (i <- nextLevelTuple) println(i)
+//  val newRDD = for (t <- nextLevelTuple) yield{
+//    for (id <- t._2) yield (id -> t._1)
+//  }
+//  for (i <- newRDD)
+//    for (j <- i)
+//    println(j)
+
+//  val p = Map(12884L -> "/1/", 1L -> "/", 10239L -> "/1/", 131567L -> "/1/", 28384L -> "/1/", 12908L -> "/1/")
+//  val newRDD = for (t <- nextLevelTuple) yield{
+//    for (id <- t._2) yield (id -> t._1)
+//  }
+//  val newMap = newRDD.flatMap(_.collect()).collectAsMap().toMap ++ p
+//  //val nextLevelIds = spark.sparkContext.parallelize(newMap.keys.toSeq)
+//  val nextLevelIds = newRDD.flatMap(_.collect().map(_._1))
+//  println(newMap)
+//  for (i <-nextLevelIds) println(i)
 }
